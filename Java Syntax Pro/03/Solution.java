2fd5c114cf0f0b83547158e4295740d1e9629257
Евгений Сагайдак <sagaydack@gmail.com>
2023-10-24 09:58:13 +0600

    Добавили 3й уровень

---
+++ Java Syntax Pro/03/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task03.task0301;
+
+import java.util.Scanner;
+
+/*
+Ввести с клавиатуры температуру на улице. Если температура меньше 0, вывести надпись "на улице холодно", иначе - вывести надпись "на улице тепло".
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        String cold = "на улице холодно";
+        String warm = "на улице тепло";
+
+    }
+}
