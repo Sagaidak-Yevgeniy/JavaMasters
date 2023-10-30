73b4b46a815a85514aace78a3c23e3e0d2ae004a
Евгений Сагайдак <sagaydack@gmail.com>
2023-10-30 09:43:39 +0600

    30.10.2023

--- Java Syntax Pro/01/Solution.java
+++ Java Syntax Pro/01/Solution.java
@@ -13,6 +13,7 @@ package com.javarush.task.task01.task0142;
 
 public class Solution {
     public static void main(String[] args) {
-      
+
+        System.out.print("Амиго очень умный");
     }
 }
